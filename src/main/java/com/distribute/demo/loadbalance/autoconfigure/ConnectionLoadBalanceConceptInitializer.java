584ba423b03bff9f96d2cf730a9aed13e530739c
王斌 <850083043@qq.com>
2023-07-24 22:26:32 +0800

    nacos获取实例的方法测试

--- src/main/java/com/distribute/demo/loadbalance/autoconfigure/ConnectionLoadBalanceConceptInitializer.java
+++ src/main/java/com/distribute/demo/loadbalance/autoconfigure/ConnectionLoadBalanceConceptInitializer.java
@@ -1,15 +1,14 @@
 package com.distribute.demo.loadbalance.autoconfigure;
 
-import com.distribute.demo.loadbalance.core.server.ConnectionServer;
 import com.distribute.demo.loadbalance.core.server.ConnectionServerManager;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -22,13 +21,21 @@ import java.util.List;
 @AllArgsConstructor
 public class ConnectionLoadBalanceConceptInitializer implements ApplicationRunner, Ordered {
 
-    @Resource
-    private ConnectionServerManager connectionServerManager;
+    private final ConnectionServerManager connectionServerManager;
+    private final DiscoveryClient discoveryClient;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
         log.info("项目启动时候开始初始化运行");
-        List<ConnectionServer> connectionServers = connectionServerManager.getConnectionServers();
+//        List<ConnectionServer> connectionServers = connectionServerManager.getConnectionServers();
+        List<String> services = discoveryClient.getServices();
+        for (String name : services) {
+            log.info("当前服务的名称：{}", name);
+        }
+//        for (ConnectionServer server : connectionServers) {
+//            log.info("当前服务的名称：{} host:{} port:{}", server.getServiceId(), server.getHost(), server.getPort());
+//        }
     }
 
     @Override
